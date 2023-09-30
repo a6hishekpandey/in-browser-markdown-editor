efe1395bf75c7055965b6dbb9f36c7cf887125de
Abhishek Pandey <pandeyabhishek0922@gmail.com>
2023-10-01 00:08:53 +0530

    Upload files to github

---
+++ middlewares.js
@@ -0,0 +1,43 @@
+const { userSchema } = require("./schemas");
+const AppError = require("./utils/AppError");
+const Document = require("./models/document");
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("danger", "Login required");
+        return res.redirect("/accounts/login");
+    }
+    next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+    const id = req.params.id;
+    const response = await Document.findById(id);
+    if (response) {
+        if (response.user._id.toString() !== req.user._id.toString()) {
+            return next(new AppError(401, "Not Authorized!"));
+        }
+    } else {
+        return next(new AppError(404, "Not found!"));
+    }
+    next();
+};
+
+module.exports.validateUser = (req, res, next) => {
+    const { email, password } = req.body;
+    const { error } = userSchema.validate({ email, password });
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new AppError(400, msg);
+    } else {
+        next();
+    }
+};
+
+module.exports.redirectToEditorIfAuthenticated = (req, res, next) => {
+    if(req.isAuthenticated()) {
+        return res.redirect("/documents/readme");
+    }
+    next();
+};
---
+++ routes/editor.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { isLoggedIn, isOwner } = require("../middlewares");
+const editorController = require("../controllers/editor");
+const catchAsyncErrors = require("../utils/catchAsyncErrors");
+const router = express.Router();
+
+router.get("/", isLoggedIn, editorController.getAllDocument);
+
+router.get("/readme", isLoggedIn, editorController.renderEditor);
+
+router
+    .route("/:id")
+    .get(isLoggedIn, isOwner, catchAsyncErrors(editorController.getDocument))
+    .put(isLoggedIn, isOwner, catchAsyncErrors(editorController.updateDocument))
+    .delete(
+        isLoggedIn,
+        isOwner,
+        catchAsyncErrors(editorController.deleteDocument)
+    );
+
+router.post(
+    "/new",
+    isLoggedIn,
+    catchAsyncErrors(editorController.createNewDocument)
+);
+
+module.exports = router;
